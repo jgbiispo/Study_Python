56401ca60d85ff7c5f76114eae343314e556a146
João Gustavo <jgbispo20@gmail.com>
2026-03-13 09:59:30 -0300

    Removando a class

--- modules/Stack.py
+++ modules/Stack.py
@@ -1,19 +1,17 @@
-class Stack:
+def is_balanced(expression: str) -> bool:
+    stack = []
 
-    @staticmethod
-    def is_balanced(expression: str) -> bool:
-        stack = []
+    for char in expression:
+        match char:
+            case "(":
+                stack.append(char)
+            case ")":
+                if not stack:
+                    return False
+                stack.pop()
 
-        for char in expression:
-            match char:
-                case "(":
-                    stack.append(char)
-                case ")":
-                    if not stack:
-                        return False
-                    stack.pop()
+    return len(stack) == 0
 
-        return len(stack) == 0
 
 def run_tests():
     test_cases = [
@@ -31,7 +29,6 @@ def run_tests():
     ]
 
     for expression, expected in test_cases:
-        result = Stack.is_balanced(expression)
+        result = is_balanced(expression)
         status = "PASS:" if result == expected else "ERROR:"
         print(f"{status} is_balanced('{expression}') = {result} (esperado: {expected})")
-
